913b719f10743018bd767ada7ae03207d198445c
Ugo Bechameil <ugo.bechameil@hackolade.com>
2024-09-06 15:11:20 +0200

    align

--- forward_engineering/api.js
+++ forward_engineering/api.js
@@ -2,34 +2,27 @@
 // const applyToInstanceHelper = require('./applyToInstanceHelper');
 
 module.exports = {
-	generateScript(data, logger, callback, app) {
-		callback(null, '');
-	},
-	generateViewScript(data, logger, callback, app) {
-		callback(new Error('Forward-Engineering of delta model on view level is not supported'));
-	},
-	generateContainerScript(data, logger, callback, app) {
-		callback(null, '');
-	},
-	applyToInstance(connectionInfo, logger, callback, app) {
-		logger.clear();
-		logger.log('info', connectionInfo, 'connectionInfo', connectionInfo.hiddenKeys);
-
-		applyToInstanceHelper
-			.applyToInstance(connectionInfo, logger, app)
-			.then(result => {
-				callback(null, result);
-			})
-			.catch(error => {
-				const err = {
-					message: error.message,
-					stack: error.stack,
-				};
-				logger.log('error', err, 'Error when applying to instance');
-				callback(err);
-			});
-	},
-	testConnection(connectionInfo, logger, callback, app) {
-		reApi.testConnection(connectionInfo, logger, callback, app).then(callback, callback);
-	},
+  generateScript(data, logger, callback, app) {
+    callback(null, "");
+  },
+  generateViewScript(data, logger, callback, app) {
+    callback(
+      new Error(
+        "Forward-Engineering of delta model on view level is not supported",
+      ),
+    );
+  },
+  generateContainerScript(data, logger, callback, app) {
+    callback(null, "");
+  },
+  applyToInstance(connectionInfo, logger, callback, app) {
+    logger.clear();
+    logger.log(
+      "info",
+      connectionInfo,
+      "connectionInfo",
+      connectionInfo.hiddenKeys,
+    );
+  },
+  testConnection(connectionInfo, logger, callback, app) {},
 };
